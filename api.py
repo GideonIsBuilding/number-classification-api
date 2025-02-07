460a52764615093da1ef5858496e8f7f071db6b7
Aleonogwe Gideon <galosikhena@gmail.com>
2025-02-07 11:36:54 +0100

    edit the app(run) command to make the app listen on all interfaces

--- api.py
+++ api.py
@@ -139,4 +139,4 @@ def classify_number():
     }), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)  # Run the Flask app in debug mode
+    app.run(host="0.0.0.0", port=5000, debug=True)  # Run the Flask app in debug mode
